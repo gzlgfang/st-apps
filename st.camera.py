7e8572417f204bf91382619d91ac4604b86b1b69
gzlgfang <lgfang@scut.edu.cn>
2022-05-04 10:13:23 +0800

    删除CV2

--- st.camera.py
+++ st.camera.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit as st
 from PIL import Image
 import numpy as np
-import cv2
+#import cv2
 picture = st.camera_input("Take a picture")
 if picture:
     st.image(picture)
@@ -19,19 +19,19 @@ if picture is not None:
 
     
     bytes_data = picture.getvalue()
-    img2 = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+   # img2 = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
     # To read image file buffer with OpenCV:
-    img_array2 = np.array(img2)
+   # img_array2 = np.array(img2)
 
     # Check the type of img_array:
     # Should output: <class 'numpy.ndarray'>
     st.write(type(img_array1))
-    st.write(type(img_array2))
+    #st.write(type(img_array2))
     # Check the shape of img_array:
     # Should output shape: (height, width, channels)
     st.write(img_array1.shape)
-    st.write(img_array2.shape)
+    #st.write(img_array2.shape)
     st.write(img_array1)
     st.write("second numpy data")
-    st.write(img_array2)
+    #st.write(img_array2)
