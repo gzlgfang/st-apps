e50325c7222b21a6fb46f2535216a9dad9202221
gzlgfang <lgfang@scut.edu.cn>
2022-07-01 12:48:11 +0800

    ENGLISH

--- pages/饼状图_page_6.py
+++ pages/饼状图_page_6.py
@@ -18,12 +18,17 @@ cy_col= ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
         '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22']
 fig=plt.figure()
 x=[0.466,0.277,0.081,0.050,0.036,0.028,0.026,0.021,0.015]
-labels=['氧','硅','铝','铁','钙','钠','钾','镁','其他'       ] 
+#labels=['氧','硅','铝','铁','钙','钠','钾','镁','其他'   ] 
+labels=['O_2','Si','Al','Fe','Ca钙','Na','K','Mg镁','Others'   ] 
 wedges,texts,autotexts=plt.pie(x,startangle=45,shadow=False,
         colors=cy_col,autopct="%3.1f%%",textprops=dict(color="w"))
-plt.text(-0.20,-1.15,"地壳元素含量表",)
+#plt.text(-0.20,-1.15,"地壳元素含量表")
+plt.text(-0.20,-1.15,"Crust element content table")
+
+
+
 #添加表格：
-title="地壳元素含量"
+title="Crust element content table"
 xValue=np.zeros(9)
 for i in range(9):
     xValue[i]=int(x[i]*100*10)/10
@@ -38,6 +43,6 @@ plt.table(loc='bottom',   # 表格在图表区的位置
           cellLoc='center',    # 表格中数据的对齐位置
           rowLabels=["含量,%"] , #表格行名称
           fontsize=12)
-plt.legend(wedges, labels,loc='center left',title="元素名称",bbox_to_anchor=(0.9,0,0.3,1))
+plt.legend(wedges, labels,loc='center left',title="Element Name",bbox_to_anchor=(0.9,0,0.3,1))
 st.pyplot(fig)
 
